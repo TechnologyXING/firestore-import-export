8df019b40016d57cdadbc98c0d687afa996b1e88
roiji <francisluigi@gmail.com>
2020-07-03 20:50:14 +0800

    ang petty ko lang sa tabs.

--- import.js
+++ import.js
@@ -13,90 +13,90 @@ let dateArray = process.argv.filter(item => item.match(reDate))[0];
 let geoArray = process.argv.filter(item => item.match(reGeo))[0];
 
 if (dateArray) {
-  dateArray = dateArray.split('=')[1].split(',');
+ 	 dateArray = dateArray.split('=')[1].split(',');
 }
 
 if (geoArray) {
-  geoArray = geoArray.split('=')[1].split(',');
+ 	 geoArray = geoArray.split('=')[1].split(',');
 }
 
 // You should replace databaseURL with your own
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: process.env.DB_URL
+	credential: admin.credential.cert(serviceAccount),
+	databaseURL: process.env.DB_URL
 });
 
 const db = admin.firestore();
 db.settings({ timestampsInSnapshots: true });
 
 fs.readFile(fileName, 'utf8', function(err, data){
-  if(err){
-    return console.log(err);
-  }
+	if(err){
+		return console.log(err);
+	}
 
-  // Turn string from file to an Array
-  if (fileName.endsWith('yaml') || fileName.endsWith('yml')) {
-    dataArray = YAML.safeLoad(data);
-  } else {
-    dataArray = JSON.parse(data);
-  }
+	// Turn string from file to an Array
+	if (fileName.endsWith('yaml') || fileName.endsWith('yml')) {
+		dataArray = YAML.safeLoad(data);
+	} else {
+		dataArray = JSON.parse(data);
+	}
 
-  updateCollection(dataArray);
+	updateCollection(dataArray);
 
 })
 
 async function updateCollection(dataArray){
-  for(const index in dataArray){
-    const collectionName = index;
-    for(const doc in dataArray[index]){
-      if(dataArray[index].hasOwnProperty(doc)){
-        await startUpdating(collectionName, doc, dataArray[index][doc]);
-      }
-    }
-  }
+	for(const index in dataArray){
+		const collectionName = index;
+		for(const doc in dataArray[index]){
+			if(dataArray[index].hasOwnProperty(doc)){
+				await startUpdating(collectionName, doc, dataArray[index][doc]);
+			}
+		}
+	}
 }
 
 function startUpdating(collectionName, doc, data){
-  // convert date from unixtimestamp
-  let parameterValid = true;
+	// convert date from unixtimestamp
+	let parameterValid = true;
 
-  // Enter date value
-  if(typeof dateArray !== 'undefined') {
-    dateArray.map(date => {
-      if (data.hasOwnProperty(date)) {
-        data[date] = new Date(data[date]._seconds * 1000);
-      } else {
-        console.log('Please check your date parameters!!!', dateArray);
-        parameterValid = false;
-      }
-    });
-  }
+	// Enter date value
+	if(typeof dateArray !== 'undefined') {
+		dateArray.map(date => {
+		if (data.hasOwnProperty(date)) {
+			data[date] = new Date(data[date]._seconds * 1000);
+		} else {
+			console.log('Please check your date parameters!!!', dateArray);
+			parameterValid = false;
+		}
+		});
+	}
 
-  // Enter geo value
-  if(typeof geoArray !== 'undefined') {
-    geoArray.map(geo => {
-      if(data.hasOwnProperty(geo)) {
-        data[geo] = new admin.firestore.GeoPoint(data[geo]._latitude, data[geo]._longitude);
-      } else {
-        console.log('Please check your geo parameters!!!', geoArray);
-        parameterValid = false;
-      }
-    })
-  }
+	// Enter geo value
+	if(typeof geoArray !== 'undefined') {
+		geoArray.map(geo => {
+			if(data.hasOwnProperty(geo)) {
+				data[geo] = new admin.firestore.GeoPoint(data[geo]._latitude, data[geo]._longitude);
+			} else {
+				console.log('Please check your geo parameters!!!', geoArray);
+				parameterValid = false;
+			}
+		})
+	}
 
-  if(parameterValid) {
-    return new Promise(resolve => {
-      db.collection(collectionName).doc(doc)
-      .set(data)
-      .then(() => {
-        console.log(`${doc} is imported successfully to firestore!`);
-        resolve('Data written!');
-      })
-      .catch(error => {
-        console.log(error);
-      });
-    });
-  } else {
-    console.log(`${doc} is not imported to firestore. Please check your parameters!`);
-  }
+	if(parameterValid) {
+		return new Promise(resolve => {
+			db.collection(collectionName).doc(doc)
+				.set(data)
+				.then(() => {
+						console.log(`${doc} is imported successfully to firestore!`);
+						resolve('Data written!');
+					})
+				.catch(error => {
+					console.log(error);
+			});
+		});
+	} else {
+		console.log(`${doc} is not imported to firestore. Please check your parameters!`);
+	}
 }
